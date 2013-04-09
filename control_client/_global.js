18e04e5631fed56dddb09cc2eed11b0c7554e93a
jonathan youngblood <jonathan.youngblood@gmail.com>
2013-04-08 21:29:31 -0500

    this might help w/ js

--- control_client/_global.js
+++ control_client/_global.js
@@ -1,6 +1,6 @@
 require.config({paths: { jquery: 'stereo/jquery-1.8.1.min' }});
 
-require(["jquery"], function($, jch) {
+require(["jquery","stereo/stereo"], function($, stereo) {
 
 	var controller = $("#js_init").data('controller');
 
